d58b9bd23135be089ec21d59d7f83d79602fa485
Hooda Akshat <hooda.akshat@syk.fi>
2022-08-19 11:29:07 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        int A=300;
+        int B=17;
+        if(A>=100&&B<=50)
+        {
+            System.out.println("1");
+        }
+        else
+        {
+            System.out.println("0");
+        }
+    }
+
+}
